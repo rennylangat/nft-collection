ce43a8d644993926a4f0b7d43e1b9eac3512a860
Renny Langat <rennylngt@gmail.com>
2022-07-04 19:59:40 +0300

    View collection on Open Sea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xf0E80e02e8511bEf354fA12f8DE03ad56372BA43";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://white-list-dapp-ten.vercel.app/";
+const METADATA_URL = "https://nft-collection-neon.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
